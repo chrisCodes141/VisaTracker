8559f4a265df1b9389d763ef91f5d81f8f19792b
Christopher Guzowski <coderchris141@gmail.com>
2024-12-20 13:00:53 -0500

    initial setup

---
+++ Springboot/visa-tracking-service/src/main/java/com/example/visa_tracking_service/VisaTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.visa_tracking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaTrackingServiceApplication.class, args);
+	}
+
+}
